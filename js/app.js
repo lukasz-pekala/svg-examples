58a486a4bde1e2fbb2d803d52afed15b5e402d37
Łukasz Pękala <lukasz.pekala@bitbucket.com>
2017-04-16 22:49:01 +0200

    Divided into separate files

--- js/app.js
+++ js/app.js
@@ -1,16 +1,26 @@
 var app = angular.module("myApp", ["ngRoute"]);
 app.config(function($routeProvider) {
     $routeProvider
+    .when("layout-and-viewbox", {
+        templateUrl : "views/layout-and-viewbox.html"
+    })
+    .when("basic-shapes", {
+        templateUrl : "views/basic-shapes.html"
+    })
+    .when("advanced-shapes", {
+        templateUrl : "views/advanced-shapes.html"
+    })
+    .when("/groups", {
+        templateUrl : "views/groups.html"
+    })
+    .when("transformations", {
+        templateUrl : "views/transformations.html"
+    })
+    .when("effects", {
+        templateUrl : "views/effects.html"
+    })
     .when("/", {
-        templateUrl : "views/main.html"
+        templateUrl : "views/basic-shapes.html"
     })
-    .when("red", {
-        templateUrl : "views/basic-shapes.htm"
-    })
-    .when("/green", {
-        templateUrl : "views/green.htm"
-    })
-    .when("/blue", {
-        templateUrl : "views/blue.htm"
-    });
+    ;
 });
